16255c92430257a22694ec3315c2f6bf6e365e1f
Kash <little.beach6287@fastmail.com>
2024-03-08 15:04:53 +0530

    add biome formatter

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 import fs from 'node:fs/promises'
-import { dirname } from "node:path";
-import Fastify from "fastify";
-import {fileURLToPath} from "node:url";
-import {createViteRuntime} from "vite";
+import { dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import Fastify from 'fastify'
+import { createViteRuntime } from 'vite'
 
 // Constants
 const isProduction = process.env.NODE_ENV === 'production'
 const port = process.env.PORT || 5173
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 const base = __dirname
 
 // Cached production assets
@@ -23,42 +23,40 @@ async function buildServer() {
   // Create http server
   const app = Fastify()
 
-// Add Vite or respective production middlewares
+  // Add Vite or respective production middlewares
   let vite
   if (!isProduction) {
     // We instantiate Vite's development server and integrate its middleware to our server.
     // ⚠️ We instantiate it only in development. (It isn't needed in production, and it
     // would unnecessarily bloat our production server.)
-    const { createServer } = await import("vite");
-    vite = (
-        await createServer({
-          server: { middlewareMode: true },
-          appType: 'custom',
-          root: base
-        })
-    );
-    const viteDevMiddleware = vite.middlewares;
+    const { createServer } = await import('vite')
+    vite = await createServer({
+      server: { middlewareMode: true },
+      appType: 'custom',
+      root: base,
+    })
+    const viteDevMiddleware = vite.middlewares
 
     // this is middleware for vite's dev server
-    app.addHook("onRequest", async (request, reply) => {
+    app.addHook('onRequest', async (request, reply) => {
       const next = () =>
-          new Promise((resolve) => {
-            viteDevMiddleware(request.raw, reply.raw, () => resolve());
-          });
-      await next();
-    });
+        new Promise(resolve => {
+          viteDevMiddleware(request.raw, reply.raw, () => resolve())
+        })
+      await next()
+    })
   } else {
     // In production, we need to serve our static assets ourselves.
     // (In dev, Vite's middleware serves our static assets.)
 
-    await app.register(import('@fastify/compress'), { global: true })
+    await app.register(import('@fastify/compress'))
     await app.register(import('@fastify/static'), {
       root: `${base}/dist/client/`,
-      prefix: "/",
-    });
+      prefix: '/',
+    })
   }
 
-// Serve HTML
+  // Serve HTML
   app.get('/', async (req, reply) => {
     try {
       const url = req.raw.url.replace(base, '')
@@ -70,7 +68,8 @@ async function buildServer() {
         template = await fs.readFile('./index.html', 'utf-8')
         template = await vite.transformIndexHtml(url, template)
         const runtime = await createViteRuntime(vite)
-        render = (await runtime.executeEntrypoint('/src/entry-server.tsx')).render
+        render = (await runtime.executeEntrypoint('/src/entry-server.tsx'))
+          .render
       } else {
         template = templateHtml
         render = (await import('./dist/server/entry-server.js')).render
@@ -79,25 +78,31 @@ async function buildServer() {
       const rendered = await render(url, ssrManifest)
 
       const html = template
-          .replace(`<!--app-head-->`, rendered.head ?? '')
-          .replace(`<!--app-html-->`, rendered.html ?? '')
+        .replace('<!--app-head-->', rendered.head ?? '')
+        .replace('<!--app-html-->', rendered.html ?? '')
 
-      reply.code(200).headers({ 'Content-Type': 'text/html;charset=utf-8', 'Cache-Control':'no-store, max-age=0' }).send(html)
+      reply
+        .code(200)
+        .headers({
+          'Content-Type': 'text/html;charset=utf-8',
+          'Cache-Control': 'no-store, max-age=0',
+        })
+        .send(html)
     } catch (e) {
       vite?.ssrFixStacktrace(e)
       reply.code(500).send(e.stack)
     }
   })
-  return app;
+  return app
 }
 
 // Start http server
-const fastify = await buildServer();
+const fastify = await buildServer()
 
 fastify.listen({ port }, (err, address) => {
   if (err) {
-    fastify.log.error(err);
-    process.exit(1);
+    fastify.log.error(err)
+    process.exit(1)
   }
   console.log(`Server started at http://localhost:${port}`)
-});
+})
